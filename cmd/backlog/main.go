ec8db11159aa0c8c7561fb933a70f3f1a854fb22
Yoshiyuki Koyanagi <moutend@gmail.com>
2020-02-04 12:56:54 +0900

    Chore

--- cmd/backlog/main.go
+++ cmd/backlog/main.go
@@ -6,7 +6,11 @@ import (
 )
 
 func main() {
+	app.RootCommand.SetOutput(os.Stdout)
+
 	if err := app.RootCommand.Execute(); err != nil {
+		app.RootCommand.SetOutput(os.Stderr)
+
 		os.Exit(-1)
 	}
 }
